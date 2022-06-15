421a276b805295687a1c4c080de50a6a05dbfcd6
Luis <acosta88co@gmail.com>
2022-06-15 20:45:56 +0200

    vercel

--- src/components/Modal/Auth/OAuthButtons.tsx
+++ src/components/Modal/Auth/OAuthButtons.tsx
@@ -15,12 +15,7 @@ const OAuthButtons: React.FC = () => {
         onClick={() => signInWithGoogle()}
       >
         {" "}
-        <Image
-          src="/images/googleLogo.png"
-          alt="logo"
-          height="20px"
-          mr={4}
-        ></Image>
+        <Image src="/images/googleLogo.png" height="20px" mr={4}></Image>
         Continuar con Google
       </Button>
       <Text align="center" color="red" fontSize="10pt">
